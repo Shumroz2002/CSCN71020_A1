d90a7444f3686337418884301a05926c3e8ba854
Shumroz Usmani <shrezorhere2002@gmail.com>
2025-02-09 19:25:17 -0500

    g

--- main.c
+++ main.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
 
+// Shumroz Usmani 
+// The two mistakes or erros are 1. It has some arbitary text at the bottom(I commented that one) 2. It misses the subtract function, its prototype and declaration as well
+
+
 void printWelcomeMenu();
 void printOptions();
 void add();
+void subtract();
 
 
 void main() {
@@ -20,6 +25,9 @@ void main() {
 	{
 	case 1:
 		add();
+		break;
+	case 2:
+		subtract();
 	}
 
 }
@@ -46,4 +54,13 @@ void add() {
 	printf("%lf + %lf = %lf\n", num1, num2, result);
 }
 
-lsadhjflkjasdlfa
+void subtract() {
+	double num1, num2, result;
+	printf("Enter the first value:");
+	scanf_s("%lf", &num1);
+	printf("Enter the second value:");
+	scanf_s("%lf", &num2);
+	result = num1 - num2;
+	printf("%lf - %lf = %lf\n", num1, num2, result);
+}
+//lsadhjflkjasdlfa
